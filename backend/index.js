6e26e65b6eb54b3a29895f239fe5e0e4237ce7e8
Yashas c gowda <yashasg434@gmail.com>
2025-01-03 21:25:47 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require('cors')
+require('dotenv').config()
+const rootRouter = require('./routes/index');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+//Routed up the main route i.e "api/v1" with rootRouter
+app.use("/api/v1",rootRouter)
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server working ${process.env.PORT} pe`);
+})
+
